e4f1d76996197d02a7e3248a1b553ee0173f0134
Alexander L. Matusiak <tuzimaster007@gmail.com>
2012-10-22 22:03:14 -0500

    version 1.0.0.01
    see included documentation for more details

---
+++ GUI/Prototype/Prototype/Prototype/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace Prototype
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.progressBar1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 583);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1092, 83);
+            this.panel1.TabIndex = 0;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(0, 3);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(873, 23);
+            this.progressBar1.TabIndex = 6;
+            this.progressBar1.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 58);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "New";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(879, 3);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            70000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(100, 20);
+            this.numericUpDown1.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(51, 58);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(1038, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Original";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(51, 29);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(1038, 20);
+            this.textBox3.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1005, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Get Slice";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.comboBox1);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1092, 28);
+            this.panel2.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(456, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(351, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Standard Deviation";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "5",
+            "7",
+            "11"});
+            this.comboBox1.Location = new System.Drawing.Point(270, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(74, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(166, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Neighborhood Size";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(575, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Process";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.chart1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 28);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1092, 555);
+            this.panel3.TabIndex = 2;
+            // 
+            // chart1
+            // 
+            chartArea3.AxisX.Title = "Index";
+            chartArea3.AxisY.Title = "Value";
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            series5.BorderWidth = 5;
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series5.Legend = "Legend1";
+            series5.Name = "Original";
+            series6.BorderWidth = 5;
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series6.Legend = "Legend1";
+            series6.Name = "Modified";
+            this.chart1.Series.Add(series5);
+            this.chart1.Series.Add(series6);
+            this.chart1.Size = new System.Drawing.Size(1092, 555);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1092, 666);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ GUI/Prototype/Prototype/Prototype/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Threading;
+
+namespace Prototype
+{
+    public partial class Form1 : Form
+    {
+        Boolean up = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        Boolean switch1 = true;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Choose the original input file.");
+            openFileDialog1.ShowDialog();
+            
+            StreamReader sr = new StreamReader(openFileDialog1.FileName);
+
+            MessageBox.Show("Choose the output file.");
+            openFileDialog1.ShowDialog();
+            StreamReader sr2 = new StreamReader(openFileDialog1.FileName);
+
+            while (!sr2.EndOfStream)
+            {
+                string line = sr2.ReadLine();
+                string[] lineb = line.Split('\t');
+
+                if (!String.IsNullOrEmpty(line) && Int16.Parse(lineb[0]) == Int16.Parse(numericUpDown1.Value.ToString()))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line2 = sr.ReadLine();
+                        string[] line2b = line2.Split('\t');
+
+                        if (!String.IsNullOrEmpty(line2) && Int16.Parse(line2b[0]) == Int16.Parse(numericUpDown1.Value.ToString()))
+                        {
+                            textBox3.Text = line;
+                            textBox4.Text = line2;
+                            sr.Close();
+                            sr2.Close();
+
+                            drawGraph(lineb, line2b);
+
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void drawGraph(string[] lineb, string[] line2b)
+        {
+            int i = 0;
+
+            double max = 0;
+            double min = 0;
+            double parseresult = 0;
+            this.chart1.Series["Original"].Points.Clear();
+            this.chart1.Series["Modified"].Points.Clear();
+            foreach (string a in lineb)
+            {
+                try
+                {
+                    parseresult = double.Parse(a);
+                    if (parseresult < min && i > 3)
+                    {
+                        min = parseresult;
+                    }
+
+                    parseresult = double.Parse(a);
+
+                    if (parseresult > max && i > 3)
+                    {
+                        max = parseresult;
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+
+            foreach (string a in line2b)
+            {
+                
+                try
+                {
+                    parseresult = double.Parse(a);
+                    if (parseresult < min && i > 3)
+                    {
+                        min = parseresult;
+                    }
+
+                    parseresult = double.Parse(a);
+
+                    if (parseresult > max && i > 3)
+                    {
+                        max = parseresult;
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+
+            foreach(string a in lineb)
+            {
+                if(!String.IsNullOrEmpty(a) && i > 3)
+                {
+                    this.chart1.Series["Original"].Points.AddXY((double)(i-3), double.Parse(a) );
+                }
+
+                i++;
+            }
+
+            chart1.Series["Original"].ChartType =
+                        SeriesChartType.FastLine;
+            chart1.Series["Original"].Color = Color.Red;
+
+            i = 0;
+
+            foreach (string a in line2b)
+            {
+                if (!String.IsNullOrEmpty(a) && i > 3)
+                {
+                    chart1.Series["Modified"].Points.AddXY((double)(i-3),double.Parse(a));
+                }
+
+                i++;
+            }
+
+            chart1.Series["Modified"].ChartType =
+            SeriesChartType.FastLine;
+            chart1.Series["Modified"].Color = Color.Blue;
+            
+            chart1.ChartAreas["ChartArea1"].AxisX.Minimum = 0;
+            chart1.ChartAreas["ChartArea1"].AxisX.Maximum = 100;
+            chart1.ChartAreas["ChartArea1"].AxisX.Interval = 1;
+
+            chart1.ChartAreas["ChartArea1"].AxisY.Minimum = min-10;
+            chart1.ChartAreas["ChartArea1"].AxisY.Maximum = max+10;
+            chart1.ChartAreas["ChartArea1"].AxisY.Interval = 1;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(comboBox1.SelectedItem.ToString()) || String.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("Please set the standard deviation or windows size before proceeding.");
+                return;
+            }
+
+            openFileDialog1.ShowDialog();
+            string inputFile = openFileDialog1.FileName;
+
+            try
+            {
+                Int16.Parse(textBox2.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Standard Deviation is not an integer!");
+                return;
+            }
+
+            Boolean pass = true;
+            try
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+                textBox2.Enabled = false;
+                textBox3.Enabled = false;
+                textBox4.Enabled = false;
+                numericUpDown1.Enabled = false;
+                comboBox1.Enabled = false;
+
+                progressBar1.Visible = true;
+
+                timer.Tick += new EventHandler(timer_Tick);
+                timer.Interval = 100;
+                timer.Enabled = true;
+
+                string[] args = new string[4];
+                args[0] = inputFile;
+                args[1] = comboBox1.SelectedItem.ToString();
+                args[2] = textBox2.Text;
+                args[3] = "True";
+                MessageBox.Show("Starting Process...");
+                backgroundWorker1.RunWorkerAsync(args);
+            }
+            catch
+            {
+            }
+        }
+
+        public static void InitializeThread(object messageObj1)
+        {
+
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            if (up && this.progressBar1.Value < 100)
+            {
+                this.progressBar1.Value += 1;
+            }
+            else
+            {
+                up = false;
+
+                if (this.progressBar1.Value > 0)
+                {
+                    this.progressBar1.Value -= 1;
+                }
+                else
+                {
+                    up = true;
+                    this.progressBar1.Value += 1;
+                }
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            System.Diagnostics.ProcessStartInfo psi = new System.Diagnostics.ProcessStartInfo();
+            psi.FileName = "C:\\Project1.exe";
+
+            psi.CreateNoWindow = true;
+            //psi.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            psi.UseShellExecute = false;
+            psi.Arguments = ((string[])e.Argument)[0] + " " + " " + ((string[])e.Argument)[1] + "  " + ((string[])e.Argument)[2];
+
+            using (Process exeProcess = Process.Start(psi))
+            {
+                exeProcess.WaitForExit();
+            }
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("Process completed successfully.");
+
+            progressBar1.Visible = false;
+            timer.Enabled = false;
+            button1.Enabled = true;
+            button2.Enabled = true;
+            textBox2.Enabled = true;
+            textBox3.Enabled = true;
+            textBox4.Enabled = true;
+            numericUpDown1.Enabled = true;
+            comboBox1.Enabled = true;
+        }
+    }
+}
